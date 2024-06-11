280fd1f6d172f28d4278225529e9db0413f30dbc
DzmitryPresnakou <7745834@gmail.com>
2024-06-11 14:22:28 +0300

    final commit

---
+++ service/src/main/java/com/dmdev/service/AppRunner.java
@@ -0,0 +1,12 @@
+package com.dmdev.service;
+
+import com.dmdev.database.dao.UserDao;
+import com.dmdev.service.service.UserService;
+
+public class AppRunner {
+
+    public static void main(String[] args) {
+        var userService = new UserService();
+        System.out.println(userService.getUser(25L));
+    }
+}
