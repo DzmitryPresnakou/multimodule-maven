8fa3fc5b7b77fe7140cb0dc349891a70107693b6
DzmitryPresnakou <7745834@gmail.com>
2024-06-13 10:49:05 +0300

    commit after fixes

--- service/src/test/java/com/dmdev/integration/ExampleIT.java
+++ service/src/test/java/com/dmdev/integration/ExampleIT.java
@@ -1,8 +1,8 @@
 package com.dmdev.integration;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ExampleIT {
 
